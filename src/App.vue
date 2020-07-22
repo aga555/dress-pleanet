9702c7a93934259fb15887b0d75cca3ad1a73c0b
akrawczyk <agammkrawczyk@gmail.com>
2020-07-22 13:07:30 +0200

    #header component style cd

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,33 @@
 <template>
-  <div id="app">
+    <div id="app">
 <app-header></app-header>
-  </div>
+        <home></home>
+    </div>
 </template>
 
 <script>
 
 
-import Header from "@/components/Header";
+    import Home from "@/components/Home";
+    import Header from "@/components/Header";
 
-export default {
-  name: 'App',
-  components: {
-    appHeader: Header
+    export default {
+        name: 'App',
+        components: {
+           appHeader: Header,
+            home: Home,
 
-  }
-}
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
 
-}
+    }
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,11 @@
 <template>
-    
+    <div class="background">
+        <div class="background-overlay">
+
+            <h3> Ready for shopping ? </h3>
+            <button class="shopping-button">Lets order</button>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -9,5 +15,34 @@
 </script>
 
 <style scoped>
+.background{
+    background: url("../assets/shop.jpg")center;
+    background-size: cover;
+    height: 30vh;
+    display: flex;
+    text-align: center;
+    align-items: center;
+    flex-direction: column;
+}
+    .background-overlay{
+        display: flex;
+        flex-direction: column;
+        text-align: center;
+        align-items: center;
 
+    }
+    h3{
+        color: white;
+        margin: 30px;
+        font-size: 30px;
+    }
+    .shopping-button{
+        margin-top: 10px;
+        padding: 10px;
+        font-size: 16px;
+        cursor: pointer;
+        background: white;
+        border-radius: 5px;
+
+    }
 </style>
