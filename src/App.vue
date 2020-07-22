3c446e915aa4443148faa47ed3acf35c61c889f6
akrawczyk <agammkrawczyk@gmail.com>
2020-07-22 12:40:17 +0200

    #header component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
-
+<app-header></app-header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+
+import Header from "@/components/Header";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    appHeader: Header
+
   }
 }
 </script>
@@ -22,6 +25,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,58 @@
+<template>
+    <header>
+        <div  class="row col-sm-12">
+            <img src="../assets/super-dress-logo.png" class="header-logo" alt="logo">
+            <h1 href="/"> super dress </h1>
+        </div>
+
+    <nav>
+        <ul>
+            <li>
+                <a href="/home"> home </a>
+            </li>
+            <span> ~</span>
+            <li>
+                <a href="/contact"> contact </a>
+            </li>
+            <span> ~</span>
+            <li>
+                <a href="/menu"> menu</a>
+            </li>
+            <span> ~</span>
+            <li>
+                <a href="/about"> about us </a>
+            </li>
+
+        </ul>
+    </nav>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped>
+h1 {
+    font-size:20px;
+    color: black;
+}
+    .header-logo{
+        width: 100px;
+    }
+    ul{
+        padding: 10px 0;
+    }
+    li{
+        display: inline-block;
+    }
+
+    @media screen and (min-width: 900px){
+.header-logo{
+    max-width: 100px;
+}
+    }
+
+</style>
