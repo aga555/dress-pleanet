ff28d3b9c5008ea2f7f46a441b00bbb96673b63b
akrawczyk <agammkrawczyk@gmail.com>
2020-07-22 22:15:48 +0200

    #Adding the login component

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="admin-wrapper">
-        <div  class="current-user">
-        <new-dress></new-dress>
+        <div class="current-user">
+            <new-dress></new-dress>
         </div>
 
         <div class="stock-wrapper">
-<h4> stock </h4>
+            <h4> stock </h4>
 
             <md-table>
                 <md-table-row>
@@ -22,9 +22,10 @@
                     <md-table-cell>Red dress</md-table-cell>
 
                     <md-table-cell>
-                    <md-button class="md-icon-button md-dense md-raised md-primary">
-                        <md-icon>cached</md-icon>
-                    </md-button></md-table-cell>
+                        <md-button class="md-icon-button md-dense md-raised md-primary">
+                            <md-icon>cached</md-icon>
+                        </md-button>
+                    </md-table-cell>
                 </md-table-row>
                 <md-table-row>
                     <md-table-cell md-numeric>2</md-table-cell>
@@ -33,16 +34,18 @@
                     <md-table-cell>
                         <md-button class="md-icon-button md-dense md-raised md-primary">
                             <md-icon>cached</md-icon>
-                        </md-button></md-table-cell>
-                    </md-table-row>
-                    <md-table-row>
-                        <md-table-cell md-numeric>3</md-table-cell>
-                        <md-table-cell>Pink dress</md-table-cell>
+                        </md-button>
+                    </md-table-cell>
+                </md-table-row>
+                <md-table-row>
+                    <md-table-cell md-numeric>3</md-table-cell>
+                    <md-table-cell>Pink dress</md-table-cell>
 
-                        <md-table-cell>
-                            <md-button class="md-icon-button md-dense md-raised md-primary">
-                                <md-icon>cached</md-icon>
-                            </md-button></md-table-cell>
+                    <md-table-cell>
+                        <md-button class="md-icon-button md-dense md-raised md-primary">
+                            <md-icon>cached</md-icon>
+                        </md-button>
+                    </md-table-cell>
 
                 </md-table-row>
             </md-table>
@@ -52,51 +55,55 @@
             <h4>Current orders</h4>
             <md-table>
                 <md-table-row>
-                    <md-table-head >order id </md-table-head>
-                    <md-table-head>item name </md-table-head>
+                    <md-table-head>order id</md-table-head>
+                    <md-table-head>item name</md-table-head>
                     <md-table-head>Price</md-table-head>
                     <md-table-head>Quantity</md-table-head>
                     <md-table-head>size</md-table-head>
-                    <md-table-head>Completed </md-table-head>
+                    <md-table-head>Completed</md-table-head>
 
                 </md-table-row>
 
                 <md-table-row>
                     <md-table-cell md-numeric>1</md-table-cell>
                     <md-table-cell>Red dress</md-table-cell>
-                    <md-table-cell> 200 </md-table-cell>
-                    <md-table-cell>1 </md-table-cell>
+                    <md-table-cell> 200</md-table-cell>
+                    <md-table-cell>1</md-table-cell>
                     <md-table-cell>38</md-table-cell>
                     <md-table-cell>
-                    <md-button class="md-icon-button md-dense md-raised md-primary">
-                        <md-icon>done</md-icon>
-                    </md-button></md-table-cell>
+                        <md-button class="md-icon-button md-dense md-raised md-primary">
+                            <md-icon>done</md-icon>
+                        </md-button>
+                    </md-table-cell>
                 </md-table-row>
                 <md-table-row>
                     <md-table-cell md-numeric>2</md-table-cell>
                     <md-table-cell>Black dress</md-table-cell>
-                    <md-table-cell> 200 </md-table-cell>
-                    <md-table-cell>2 </md-table-cell>
+                    <md-table-cell> 200</md-table-cell>
+                    <md-table-cell>2</md-table-cell>
                     <md-table-cell>40</md-table-cell>
-                    <md-table-cell> <md-button class="md-icon-button md-dense md-raised md-primary">
-                        <md-icon>done</md-icon>
-                    </md-button>
+                    <md-table-cell>
+                        <md-button class="md-icon-button md-dense md-raised md-primary">
+                            <md-icon>done</md-icon>
+                        </md-button>
                     </md-table-cell>
                 </md-table-row>
             </md-table>
-            </div>
+        </div>
 
-
-
+<login></login>
     </div>
 </template>
 
 <script>
     import NewDress from "@/components/NewDress";
+    import Login from "@/components/Login";
+
     export default {
         name: "Admin",
-        components:{
-            newDress:NewDress
+        components: {
+            newDress: NewDress,
+            login: Login
         }
 
     }
---
+++ src/components/Login.vue
@@ -0,0 +1,31 @@
+<template>
+    <form novalidate class="md-layout" >
+        <md-card class="md-layout-item md-size-50 md-small-size-100">
+            <md-card-header>
+                <div class="md-title">Login</div>
+            </md-card-header>
+
+            <md-card-content>
+                <md-field >
+                    <label for="email">Email</label>
+                    <md-input type="email" name="email" id="email"   />
+                </md-field>
+                <md-field>
+                    <label>Password toggle</label>
+                    <md-input  type="password"></md-input>
+                </md-field>
+            </md-card-content>
+            <md-button class="md-active">sign in</md-button>
+        </md-card>
+    </form>
+</template>
+
+<script>
+    export default {
+        name: "Login"
+    }
+</script>
+
+<style scoped>
+
+</style>
