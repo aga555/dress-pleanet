2423d73f7c4ae8908b8d873b97c5dec0a5ff4b32
akrawczyk <agammkrawczyk@gmail.com>
2020-07-22 17:07:37 +0200

    #Looping through menu items

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -2,45 +2,53 @@
     <div class="menu-wrapper">
         <!--menu-->
         <div class="menu">
+            <md-layout md-flex="50" v-for="(item) in getMenuItems" :key="item.id">
 
-            <md-card>
-                <md-card-media>
-                    <img src="../assets/dress-black.jpg" alt="black-dress" style="width:200px">
-                </md-card-media>
+                <md-card>
+                    <md-card-media>
 
-                <md-card-header>
-                    <div class="md-title"> Black dress</div>
-                    <div class="md-subhead">size : s </div>
-                </md-card-header>
+                    </md-card-media>
 
-                <md-card-actions>
-                    <md-button>Add to basket </md-button>
+                    <md-card-header>
+                        <div class="md-title"> {{item.name }}</div>
+                        <div class="md-subhead">{{item.price}}</div>
+                        <div class="md-subhead" v-for="(option,index) in item.options" :key="option[index]"> size: {{option.size}}</div>
+                    </md-card-header>
 
-                </md-card-actions>
+                    <md-card-actions>
+                        <md-button>Add to basket</md-button>
 
-                <md-card-content>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
-                </md-card-content>
-            </md-card>
-            <md-card>
-                <md-card-media>
-                    <img src="../assets/dress-green.jpg" alt="dress" style="width:200px">
-                </md-card-media>
+                    </md-card-actions>
 
-                <md-card-header>
-                    <div class="md-title"> Green dress </div>
-                    <div class="md-subhead">size : s </div>
-                </md-card-header>
+                    <md-card-content>
+                     {{item.description}}
+                    </md-card-content>
+                </md-card>
+            </md-layout>
+              <!--  <md-card>
+                    <md-card-media>
+                        <img src="{{item.img}}" alt="dress" style="width:200px">
+                    </md-card-media>
 
-                <md-card-actions>
-                    <md-button>Add to basket </md-button>
+                    <md-card-header>
+                        <div class="md-title"> Green dress</div>
 
-                </md-card-actions>
 
-                <md-card-content>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
-                </md-card-content>
-            </md-card>
+                    </md-card-header>
+
+                    <md-card-actions>
+                        <md-button>Add to basket</md-button>
+
+                    </md-card-actions>
+
+                    <md-card-content>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio.
+                        Dolores,
+                        sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus
+                        sint.
+                        Excepturi.
+                    </md-card-content>
+                </md-card>-->
 
 
         </div>
@@ -59,7 +67,67 @@
 
 
     export default {
-        name: "Menu"
+        name: "Menu",
+        data: function () {
+            return {
+                getMenuItems: {
+
+                    1: {
+                        id: 1,
+                        name: " Black dress",
+                        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.",
+                        options: [
+                            {size: 36},
+                            {size: 38},
+                            {size: 40}
+                        ],
+                        price: 200,
+                        //url: '../assets/dress-green.jpg'
+                      //  url: "https://pixabay.com/pl/photos/kobieta-m%C5%82ody-r%C3%B3%C5%BCowy-%C5%82adny-pi%C4%99kny-3075704/"
+                    },
+                    2: {
+                        id: 2,
+                        name: " Green dress",
+                        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.",
+                        options: [
+                            {size: 36},
+                            {size: 38},
+                            {size: 40}
+                        ],
+                        price: 200,
+                        img: "../assets/dress-green.jpg"
+                    },
+                    3: {
+                        id: 3,
+                        name: " Pink dress",
+                        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.",
+                        options: [
+                            {size: 36},
+                            {size: 38},
+                            {size: 40}
+                        ],
+                        price: 200,
+                        img: "../assets/pink-dress.jpg"
+                    },
+                    4: {
+                        id: 4,
+                        name: " Red dress",
+                        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.",
+                        options: [
+                            {size: 36},
+                            {size: 38},
+                            {size: 40},
+                            {size: 42}
+                        ],
+                        price: 200,
+                        img: "../assets/red-dress.jpg"
+                    },
+                }
+
+
+            }
+        }
+
     }
 </script>
 
@@ -70,18 +138,21 @@
         display: inline-block;
         vertical-align: top;
     }
-    .menu-wrapper{
+
+    .menu-wrapper {
         display: flex;
         flex-direction: column;
     }
-    @media  screen and (min-width: 900px) {
-        .menu-wrapper{
+
+    @media screen and (min-width: 900px) {
+        .menu-wrapper {
             display: flex;
             flex-direction: row;
             justify-content: space-between;
         }
-     .menu{
-         width: 60vw;
-     }
+
+        .menu {
+            width: 60vw;
+        }
     }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
+import {MdButton, MdCard, MdContent, MdField, MdTabs} from 'vue-material/dist/components'
+import 'vue-material/dist/theme/default.css'
 
 Vue.config.productionTip = false
 
+Vue.use(MdButton);
+Vue.use(MdContent);
+Vue.use(MdTabs);
+Vue.use(MdCard);
 
+Vue.use(MdField);
 Vue.use(VueMaterial)
 new Vue({
   render: h => h(App),
